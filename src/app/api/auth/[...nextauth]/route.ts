feef70524ad2a11bf312a08590ecd0a3e1ddb6d1
Robson <robson14br@outlook.com>
2024-07-27 23:45:36 -0300

    feat Credentials

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,39 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
+  pages: {
+    signIn: "/",
+  },
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
+
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "email"},
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if(!credentials) {
+          return null
+        }
+        
+        if(credentials.email === "robson@gmail.com" && credentials.password === "123") {
+          return {
+            id: "1",
+            name: "Robson",
+            email: "robson@gmail.com"
+          }
+        }
+        return null
+      },
+    }),
+
   ],
 });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,66 @@
 "use client";
 
-import Github from "next-auth/providers/github";
 import { signIn } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
 
 export default function Home() {
+  const searchParams = useSearchParams();
+  const error = searchParams.get("error");
+
+  async function login(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    const data = {
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+
+    signIn("credentials", {
+      ...data,
+      callbackUrl: "/dashboard",
+    });
+  }
+
   return (
-    <main className="flex justify-center items-center h-screen">
-      <button
-        className="btn btn-primary"
-        onClick={() => signIn("github", { callbackUrl: "/dashboard" })}
-      >
-        Login com GitHub
-      </button>
+    <main className="">
+      <div className="h-screen flex justify-center items-center bg-slate-600 px-5">
+        <form
+          onSubmit={login}
+          className="bg-white p-12 rounded-lg w-96 max-w-full flex justify-center items-center flex-col gap-2"
+        >
+          <h2 className="font-bold text-xl mb-3">Faça seu Login</h2>
+          <input
+            name="email"
+            type="email"
+            placeholder="Email"
+            className="input input-primary w-full"
+            required
+          />
+          <input
+            name="password"
+            type="password"
+            placeholder="Senha"
+            className="input input-primary w-full"
+            required
+          />
+          <button className="btn btn-primary w-full" type="submit">
+            Login
+          </button>
+
+          <button
+            className="btn btn-primary w-full mt-2"
+            type="button"
+            onClick={() => signIn("github", { callbackUrl: "/dashboard" })}
+          >
+            Login com GitHub
+          </button>
+
+          {error === "CredentialsSignin" && (
+            <div className="text-red-500 mt-2">Erro no Login</div>
+          )}
+        </form>
+      </div>
     </main>
   );
 }
